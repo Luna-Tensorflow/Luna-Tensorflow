6b8135953c5c8fac56864d0196e3d6b0b3e02914
Wojciech Ciszewski <wojtrz13@gmail.com>
2018-11-06 16:09:26 +0100

    Calculating Ax+b, where A, b are variables and x has a placeholder

--- src/Graph.h
+++ src/Graph.h
@@ -40,10 +40,67 @@ class Graph {
 public:
     Graph() {
         graph = TF_NewGraph();
+        options = TF_NewSessionOptions();
+        TF_Status *status = TF_NewStatus();
+        session = TF_NewSession(graph, options, status);
+        delete_or_throw(status);
     }
 
     template<TF_DataType dtype> TF_Output make_variable(std::string name, int n, int m) {
         TF_OperationDescription *desc = TF_NewOperation(graph, "Variable", name.c_str());
+
+        int64_t dims[] = {n,m};
+
+        TF_SetAttrShape(desc, "shape", dims, 2);
+        TF_SetAttrType(desc, "dtype", dtype);
+        TF_Status* status = TF_NewStatus();
+        TF_Operation* operation = TF_FinishOperation(desc, status);
+
+        delete_or_throw(status);
+
+        TF_Output output = {
+                .oper = operation,
+                .index = 0
+        };
+        return output;
+    }
+
+    TF_Operation* make_variable_init(std::string name, TF_Output variable, const Tensor2d &tensor) {
+        TF_Output value = make_constant<TF_FLOAT>(name + "_val", tensor);
+        TF_OperationDescription *desc = TF_NewOperation(graph, "Assign", name.c_str());
+
+        TF_AddInput(desc, variable);
+        TF_AddInput(desc, value);
+
+        TF_Status *status = TF_NewStatus();
+        TF_Operation* operation = TF_FinishOperation(desc, status);
+        delete_or_throw(status);
+
+        return operation;
+    }
+
+    template<TF_DataType dtype> TF_Output make_constant(std::string name, const Tensor2d& tensor) {
+        TF_OperationDescription *desc = TF_NewOperation(graph, "Const", name.c_str());
+
+        TF_SetAttrType(desc, "dtype", dtype);
+        TF_Status* status = TF_NewStatus();
+        TF_SetAttrTensor(desc, "value", tensor.get_underlying(), status);
+        delete_or_throw(status);
+
+        status = TF_NewStatus();
+        TF_Operation *operation = TF_FinishOperation(desc, status);
+        delete_or_throw(status);
+
+        TF_Output output = {
+                .oper = operation,
+                .index = 0
+        };
+
+        return output;
+    }
+
+    template<TF_DataType dtype> TF_Output make_placeholder(std::string name, int n, int m) {
+        TF_OperationDescription *desc = TF_NewOperation(graph, "Placeholder", name.c_str());
 
         int64_t dims[] = {n,m};
 
@@ -78,19 +135,31 @@ public:
         return output;
     }
 
+    TF_Output make_matmul(std::string name, TF_Output a, TF_Output b) {
+        TF_OperationDescription* desc = TF_NewOperation(graph, "MatMul", name.c_str());
+
+        TF_AddInput(desc, a);
+        TF_AddInput(desc, b);
+
+        TF_Status* status = TF_NewStatus();
+        TF_Operation* operation = TF_FinishOperation(desc, status);
+        delete_or_throw(status);
+
+        TF_Output output = {
+                .oper = operation,
+                .index = 0
+        };
+        return output;
+    }
+
     std::vector<Tensor2d> run_session(
             const std::vector<TF_Output> &inputs,
             const std::vector<Tensor2d> &input_values,
-            const std::vector<TF_Output> &outputs
+            const std::vector<TF_Output> &outputs,
+            const std::vector<TF_Operation*> &operations
     ) {
 
         if (inputs.size() != input_values.size()) throw std::invalid_argument("Input vectors must have same length");
-
-        TF_SessionOptions* options = TF_NewSessionOptions();
-        TF_Status *status = TF_NewStatus();
-        TF_Session* session = TF_NewSession(graph, options, status);
-
-        delete_or_throw(status);
 
         std::vector<TF_Tensor*> input_t;
         for (const Tensor2d& t : input_values) {
@@ -99,24 +168,14 @@ public:
 
         std::vector<TF_Tensor*> output_t(outputs.size());
 
-        status = TF_NewStatus();
+        TF_Status* status = TF_NewStatus();
         TF_SessionRun(session,
                       nullptr,
                       inputs.data(), input_t.data(), inputs.size(), // we provide our input tensors by setting the outputs of the variable nodes
                       outputs.data(), output_t.data(), outputs.size(), // we specify which outputs we want to learn
-                      nullptr, 0, // we could provide some nodes that we want to execute, but don't need their output (it makes sense because of nodes like "Assign")
+                      operations.data(), operations.size(), // we can provide some nodes that we want to execute, but don't need their output (it makes sense because of nodes like "Assign")
                       nullptr,
                       status);
-        void* p = TF_TensorData(output_t[0]);
-        delete_or_throw(status);
-
-        status = TF_NewStatus();
-        TF_CloseSession(session, status);
-        delete_or_throw(status);
-
-        TF_DeleteSessionOptions(options);
-        status = TF_NewStatus();
-        TF_DeleteSession(session, status);
         delete_or_throw(status);
 
         std::vector<Tensor2d> output_values;
@@ -127,10 +186,22 @@ public:
     ~Graph() {
         TF_DeleteGraph(graph);
         graph = nullptr;
+        TF_Status* status = TF_NewStatus();
+        TF_CloseSession(session, status);
+        delete_or_throw(status);
+
+        TF_DeleteSessionOptions(options);
+        options = nullptr;
+        status = TF_NewStatus();
+        TF_DeleteSession(session, status);
+        delete_or_throw(status);
+        session = nullptr;
     }
 
 private:
     TF_Graph *graph;
+    TF_Session *session;
+    TF_SessionOptions *options;
 };
 
 #endif //TF_EXAMPLE_OPS_H
--- src/main.cpp
+++ src/main.cpp
@@ -7,43 +7,57 @@
 using namespace std;
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-
-    std::vector<Tensor2d> input_tensors;
-    input_tensors.emplace_back(n, m);
-    input_tensors.emplace_back(n, m);
-    for (int k = 0; k < 2; ++k) {
-        cout << "Tensor " << k << endl;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                float x;
-                cin >> x;
-                input_tensors[k](i, j) = x;
-            }
-        }
-    }
+    int n;
+    cin >> n;
 
     Graph graph;
 
-    auto a = graph.make_variable<TF_FLOAT>("Var1", n, m);
-    auto b = graph.make_variable<TF_FLOAT>("Var2", n, m);
-
-    auto added = graph.make_addition("Add1", a, b);
-
-    std::vector<Tensor2d> output_tensors = graph.run_session(
-            {a, b},
-            input_tensors,
-            {added}
-            );
-
-    cout << "Output:\n";
+    vector<Tensor2d> inputs;
+    inputs.push_back(Tensor2d(n, 1));
+    Tensor2d input(n, 1);
+    cout << "Input tensor: \n";
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            cout << output_tensors[0](i, j) << " ";
-        }
-        cout << "\n";
+        cin >> inputs[0](i, 0);
     }
 
+    auto A = graph.make_variable<TF_FLOAT>("A", n, n);
+    auto b = graph.make_variable<TF_FLOAT>("b", n, 1);
+    auto x = graph.make_placeholder<TF_FLOAT>("x", n, 1);
+
+    auto Ax = graph.make_matmul("Ax", A, x);
+    auto Ax_plus_b = graph.make_addition("Ax_plus_b", Ax, b);
+
+    Tensor2d A_init(n, n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            A_init(i, j) = i * n + j + 2;
+        }
+    }
+    TF_Operation* A_init_op = graph.make_variable_init("A_init", A, A_init);
+
+    Tensor2d b_init(n, 1);
+    for (int i = 0; i < n; ++i) {
+        b_init(i, 0) = i + 3;
+    }
+    TF_Operation* b_init_op = graph.make_variable_init("b_init", b, b_init);
+
+    graph.run_session(
+            {},
+            {},
+            {},
+            {A_init_op, b_init_op}
+            );
+
+    std::vector<Tensor2d> output_tensors = graph.run_session(
+            {x},
+            inputs,
+            {Ax_plus_b},
+            {}
+            );
+
+    cout << "Output: tensor\n";
+    for (int i = 0; i < n; ++i) {
+        cout << output_tensors[0](i, 0) << "\n";
+    }
     return 0;
 }
