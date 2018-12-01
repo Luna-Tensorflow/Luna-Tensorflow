d726acb2830540bc9ccd5e93a8c2061ac4170513
Wojciech Ciszewski <wojtrz13@gmail.com>
2018-12-01 14:31:13 +0100

    (#17) Copied LifetimeManager to API

---
+++ API/native_libs/src/LifeTimeManger.cpp
@@ -0,0 +1,11 @@
+//Source: https://github.com/luna/Dataframes/blob/787b733241cd833d140e42effeeede0a2e81fa65/native_libs/src/LifetimeManager.cpp
+
+#include "LifeTimeManager.h"
+
+LifetimeManager::LifetimeManager()
+{
+}
+
+LifetimeManager::~LifetimeManager()
+{
+}
