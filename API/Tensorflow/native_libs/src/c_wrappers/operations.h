e3c73cc1c7ed20bb551ba46984c2e03065d0593a
Radosław Waśko <programistagd@gmail.com>
2018-12-06 22:25:04 +0100

    (#26) Added TFL_API macro.

--- API/Tensorflow/native_libs/src/c_wrappers/operations.h
+++ API/Tensorflow/native_libs/src/c_wrappers/operations.h
@@ -7,6 +7,7 @@
 
 #include <cstddef>
 #include <tensorflow/c/c_api.h>
+#include "common.h"
 
 template <TF_DataType DataTypeLabel> class Tensor;
 template <TF_DataType DataTypeLabel> class Operation;
@@ -16,21 +17,21 @@ extern "C"
 {
 #endif
 
-Operation<TF_FLOAT>* make_op_const_float(Tensor<TF_FLOAT>* tensor);
-Operation<TF_INT32>* make_op_const_int(Tensor<TF_INT32>* tensor);
+TFL_API Operation<TF_FLOAT>* make_op_const_float(Tensor<TF_FLOAT>* tensor);
+TFL_API Operation<TF_INT32>* make_op_const_int(Tensor<TF_INT32>* tensor);
 
-Operation<TF_FLOAT>* make_op_binary_float(const char* name, Operation<TF_FLOAT>* a, Operation<TF_FLOAT>* b);
-Operation<TF_INT32>* make_op_binary_int(const char* name, Operation<TF_INT32>* a, Operation<TF_INT32>* b);
+TFL_API Operation<TF_FLOAT>* make_op_binary_float(const char* name, Operation<TF_FLOAT>* a, Operation<TF_FLOAT>* b);
+TFL_API Operation<TF_INT32>* make_op_binary_int(const char* name, Operation<TF_INT32>* a, Operation<TF_INT32>* b);
 
-size_t operation_hashcode_float(Operation<TF_FLOAT>*);
-size_t operation_hashcode_int(Operation<TF_INT32>*);
+TFL_API size_t operation_hashcode_float(Operation<TF_FLOAT>*);
+TFL_API size_t operation_hashcode_int(Operation<TF_INT32>*);
 
-Tensor<TF_FLOAT>* eval_op_float(Operation<TF_FLOAT>* op);
-Tensor<TF_INT32>* eval_op_int(Operation<TF_INT32>* op);
+TFL_API Tensor<TF_FLOAT>* eval_op_float(Operation<TF_FLOAT>* op);
+TFL_API Tensor<TF_INT32>* eval_op_int(Operation<TF_INT32>* op);
 
-Operation<TF_FLOAT>* make_op_partial_derivative(Operation<TF_FLOAT>* a, Operation<TF_FLOAT>* b);
+TFL_API Operation<TF_FLOAT>* make_op_partial_derivative(Operation<TF_FLOAT>* a, Operation<TF_FLOAT>* b);
 
-Tensor<TF_FLOAT>** batch_eval_op_float(Operation<TF_FLOAT>** ops, size_t count);
+TFL_API Tensor<TF_FLOAT>** batch_eval_op_float(Operation<TF_FLOAT>** ops, size_t count);
 
 #ifdef __cplusplus
 };
