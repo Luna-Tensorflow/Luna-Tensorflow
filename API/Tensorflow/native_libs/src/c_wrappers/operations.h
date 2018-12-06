86afaa9173badcfead1b63077086cb3dd39185ed
Mateusz Goślinowski <mateusz.goslinowski@gmail.com>
2018-12-06 13:20:11 +0100

    (#30) Implemented C side of batchEval

--- API/Tensorflow/native_libs/src/c_wrappers/operations.h
+++ API/Tensorflow/native_libs/src/c_wrappers/operations.h
@@ -11,18 +11,29 @@
 template <TF_DataType DataTypeLabel> class Tensor;
 template <TF_DataType DataTypeLabel> class Operation;
 
-extern "C" Operation<TF_FLOAT>* make_op_const_float(Tensor<TF_FLOAT>* tensor);
-extern "C" Operation<TF_INT32>* make_op_const_int(Tensor<TF_INT32>* tensor);
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-extern "C" Operation<TF_FLOAT>* make_op_binary_float(const char* name, Operation<TF_FLOAT>* a, Operation<TF_FLOAT>* b);
-extern "C" Operation<TF_INT32>* make_op_binary_int(const char* name, Operation<TF_INT32>* a, Operation<TF_INT32>* b);
+Operation<TF_FLOAT>* make_op_const_float(Tensor<TF_FLOAT>* tensor);
+Operation<TF_INT32>* make_op_const_int(Tensor<TF_INT32>* tensor);
 
-extern "C" size_t operation_hashcode_float(Operation<TF_FLOAT>*);
-extern "C" size_t operation_hashcode_int(Operation<TF_INT32>*);
+Operation<TF_FLOAT>* make_op_binary_float(const char* name, Operation<TF_FLOAT>* a, Operation<TF_FLOAT>* b);
+Operation<TF_INT32>* make_op_binary_int(const char* name, Operation<TF_INT32>* a, Operation<TF_INT32>* b);
 
-extern "C" Tensor<TF_FLOAT>* eval_op_float(Operation<TF_FLOAT>* op);
-extern "C" Tensor<TF_INT32>* eval_op_int(Operation<TF_INT32>* op);
+size_t operation_hashcode_float(Operation<TF_FLOAT>*);
+size_t operation_hashcode_int(Operation<TF_INT32>*);
 
-extern "C" Operation<TF_FLOAT>* make_op_partial_derivative(Operation<TF_FLOAT>* a, Operation<TF_FLOAT>* b);
+Tensor<TF_FLOAT>* eval_op_float(Operation<TF_FLOAT>* op);
+Tensor<TF_INT32>* eval_op_int(Operation<TF_INT32>* op);
+
+Operation<TF_FLOAT>* make_op_partial_derivative(Operation<TF_FLOAT>* a, Operation<TF_FLOAT>* b);
+
+Tensor<TF_FLOAT>** batch_eval_op_float(Operation<TF_FLOAT>** ops, size_t count);
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif //FFITESTHELPER_OPERATIONS_H
