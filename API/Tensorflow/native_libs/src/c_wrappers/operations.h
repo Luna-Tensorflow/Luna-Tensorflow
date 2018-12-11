91c7eb4799e88642d9bbe83a22779abc0e6c1a5d
tomaszmadej <tomasz.madej.poczta@gmail.com>
2018-12-11 20:14:32 +0100

    Unary Operation class, sigmoid

--- API/Tensorflow/native_libs/src/c_wrappers/operations.h
+++ API/Tensorflow/native_libs/src/c_wrappers/operations.h
@@ -23,6 +23,8 @@ TFL_API Operation<TF_INT32>* make_op_const_int(Tensor<TF_INT32>* tensor);
 TFL_API Operation<TF_FLOAT>* make_op_binary_float(const char* name, Operation<TF_FLOAT>* a, Operation<TF_FLOAT>* b);
 TFL_API Operation<TF_INT32>* make_op_binary_int(const char* name, Operation<TF_INT32>* a, Operation<TF_INT32>* b);
 
+TFL_API Operation<TF_FLOAT>* make_op_unary_float(const char* name, Operation<TF_FLOAT>* a);
+
 TFL_API size_t operation_hashcode_float(Operation<TF_FLOAT>*);
 TFL_API size_t operation_hashcode_int(Operation<TF_INT32>*);
 
