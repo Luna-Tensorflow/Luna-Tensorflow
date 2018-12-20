4206b82fcc35bf3a3b2f33c405f498d0c706f4b3
Wojciech Ciszewski <wojtrz13@gmail.com>
2018-12-20 08:59:46 +0100

    Moved TensorFlow log suppression.

--- API/Tensorflow/native_libs/src/graph/GraphSession.cpp
+++ API/Tensorflow/native_libs/src/graph/GraphSession.cpp
@@ -1,10 +1,16 @@
 //
 // Created by mateusz on 04.12.18.
 //
+
+#include <cstdlib>
+
 #include "GraphSession.h"
 
 GraphSession::GraphSession()
 {
+	char suppress_tf_log[] = "TF_CPP_MIN_LOG_LEVEL=3";
+	putenv(suppress_tf_log);
+
 	graph = TF_NewGraph();
 	options = TF_NewSessionOptions();
 	session = run_with_status<TF_Session*>(std::bind(TF_NewSession, graph, options, std::placeholders::_1));
