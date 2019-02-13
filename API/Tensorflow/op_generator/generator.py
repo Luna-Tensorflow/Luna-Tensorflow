87b5891d25b7a156410014acc68e3b2a5ef47162
Wojciech Ciszewski <wojtrz13@gmail.com>
2019-02-13 01:05:15 +0100

    Example usage of op_def proto and TF_GetAllOpList() in Python.

---
+++ API/Tensorflow/op_generator/generator.py
@@ -0,0 +1,27 @@
+from tensorflow.core.framework import op_def_pb2
+from cffi import FFI
+
+ffi = FFI()
+ffi.cdef("""
+    typedef struct TF_Buffer {
+        const void* data;
+        size_t length;
+        void (*data_deallocator)(void* data, size_t length);
+    } TF_Buffer;
+
+    TF_Buffer* TF_GetAllOpList();
+    
+    void TF_DeleteBuffer(TF_Buffer*);
+""")
+
+TF = ffi.dlopen('tensorflow')
+
+ops = TF.TF_GetAllOpList()
+
+opList = op_def_pb2.OpList()
+opList.ParseFromString(ffi.buffer(ops.data, ops.length)[:])
+
+for op in opList.op:
+    print(op.name)
+
+TF.TF_DeleteBuffer(ops)
