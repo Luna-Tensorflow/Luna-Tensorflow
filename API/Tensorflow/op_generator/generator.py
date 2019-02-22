ea21a36d6cf5e36ad0483796c0fd6a65d6a9e9a7
Wojciech Ciszewski <wojtrz13@gmail.com>
2019-02-22 13:10:18 +0100

    Basic ops generation, without any attributes.

--- API/Tensorflow/op_generator/generator.py
+++ API/Tensorflow/op_generator/generator.py
@@ -1,6 +1,30 @@
 from tensorflow.core.framework import op_def_pb2
 from cffi import FFI
 
+generated_ops_file = '../src/GeneratedOps.luna'
+file_header = """import Tensorflow.CWrappers.Operations
+import Tensorflow.Types
+import Tensorflow.Operations"""
+
+
+def op_code(name, nargs, noutputs):
+    head = 'def ' + name.lower().replace('_', 'underscore')  # TODO: what to do with this?
+    for i in range(nargs):
+        head += ' in' + str(i)
+    head += ':'
+    make_wrappers = 'wrappers = makeOutputWrappers "' + name + '" ['
+    for i in range(nargs):
+        make_wrappers += 'in' + str(i)
+        if i < nargs - 1:
+            make_wrappers += ', '
+    make_wrappers += '] ' + str(noutputs) + ' ""'
+    if noutputs == 1:
+        return_line = 'TFOutput wrappers.head.get'
+    else:
+        return_line = 'wrappers.each (wrapper: TFOutput wrapper)'
+    return head + '\n    ' + make_wrappers + '\n    ' + return_line
+
+
 ffi = FFI()
 ffi.cdef("""
     typedef struct TF_Buffer {
@@ -21,9 +45,11 @@ ops = TF.TF_GetAllOpList()
 opList = op_def_pb2.OpList()
 opList.ParseFromString(ffi.buffer(ops.data, ops.length)[:])
 
-for op in opList.op:
-    #print(op.HasField("deprecation"))
-    if len(op.output_arg) > 1:
-        print(op.name)
+with open(generated_ops_file, 'w+') as f:
+    f.write(file_header)
+    for op in opList.op:
+        if not op.is_stateful:
+            f.write('\n\n' + op_code(op.name, len(op.input_arg), len(op.output_arg)))
+    f.write('\n')
 
 TF.TF_DeleteBuffer(ops)
