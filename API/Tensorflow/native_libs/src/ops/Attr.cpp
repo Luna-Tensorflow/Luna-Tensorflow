853c4a0d4f48020123bb5ddad29799925f2f6ea6
tomaszmadej <tomasz.madej.poczta@gmail.com>
2019-03-09 20:27:18 +0100

    (#56) Hashes of Attr subclasses, hashing TF_DataType

--- API/Tensorflow/native_libs/src/ops/Attr.cpp
+++ API/Tensorflow/native_libs/src/ops/Attr.cpp
@@ -7,8 +7,6 @@
 #include "../helpers/utils.h"
 
 Attr::Attr(const std::string &name) : name(name) {
-    static size_t hash_ctr = 0;
-    hash = ++hash_ctr;
 }
 
 size_t Attr::hashcode() const {
@@ -16,6 +14,9 @@ size_t Attr::hashcode() const {
 }
 
 AttrType::AttrType(const std::string &name, TF_DataType type) : Attr(name), type(type) {
+    hash = std::hash<std::string>()(name);
+    size_t type_hash = std::hash<size_t>()(static_cast<size_t>(type));
+    hash = hash_combine(hash, type_hash);
 }
 
 void AttrType::set(TF_OperationDescription *desc) const {
@@ -23,6 +24,11 @@ void AttrType::set(TF_OperationDescription *desc) const {
 }
 
 AttrTypeList::AttrTypeList(const std::string &name, const std::vector<TF_DataType> &types) : Attr(name), types(types) {
+    hash = std::hash<std::string>()(name);
+    for (auto type : types) {
+        size_t type_hash = std::hash<size_t>()(static_cast<size_t>(type));
+        hash = hash_combine(hash, type_hash);
+    }
 }
 
 void AttrTypeList::set(TF_OperationDescription *desc) const {
@@ -30,6 +36,10 @@ void AttrTypeList::set(TF_OperationDescription *desc) const {
 }
 
 AttrShape::AttrShape(const std::string &name, const std::vector<int64_t> &dims) : Attr(name), dims(dims) {
+    hash = std::hash<std::string>()(name);
+    for (auto d : dims) {
+        hash = hash_combine(hash, std::hash<int64_t>()(d));
+    }
 }
 
 void AttrShape::set(TF_OperationDescription *desc) const {
@@ -37,6 +47,12 @@ void AttrShape::set(TF_OperationDescription *desc) const {
 }
 
 AttrShapeList::AttrShapeList(const std::string &name, std::vector<std::vector<int64_t>> dims) : Attr(name), dims(std::move(dims)) {
+    hash = std::hash<std::string>()(name);
+    for (auto &dim : dims) {
+        for (auto d : dim) {
+            hash = hash_combine(hash, std::hash<int64_t>()(d));
+        }
+    }
 }
 
 void AttrShapeList::set(TF_OperationDescription *desc) const {
@@ -52,6 +68,8 @@ void AttrShapeList::set(TF_OperationDescription *desc) const {
 }
 
 AttrTensor::AttrTensor(const std::string &name, const Tensor &tensor) : Attr(name), tensor(tensor) {
+    hash = std::hash<std::string>()(name);
+    hash = hash_combine(hash, tensor.hashcode());
 }
 
 void AttrTensor::set(TF_OperationDescription *desc) const {
@@ -61,6 +79,10 @@ void AttrTensor::set(TF_OperationDescription *desc) const {
 
 AttrTensorList::AttrTensorList(const std::string &name, const std::vector<Tensor> &tensors) : Attr(name),
         tensors(tensors) {
+    hash = std::hash<std::string>()(name);
+    for (auto &tensor : tensors) {
+        hash = hash_combine(hash, tensor.hashcode());
+    }
 }
 
 void AttrTensorList::set(TF_OperationDescription *desc) const {
@@ -73,6 +95,8 @@ void AttrTensorList::set(TF_OperationDescription *desc) const {
 }
 
 AttrInt::AttrInt(const std::string &name, int64_t value) : Attr(name), value(value) {
+    hash = std::hash<std::string>()(name);
+    hash = hash_combine(hash, std::hash<int64_t>()(value));
 }
 
 void AttrInt::set(TF_OperationDescription *desc) const {
@@ -80,6 +104,10 @@ void AttrInt::set(TF_OperationDescription *desc) const {
 }
 
 AttrIntList::AttrIntList(const std::string &name, const std::vector<int64_t> &values) : Attr(name), values(values) {
+    hash = std::hash<std::string>()(name);
+    for (auto value : values) {
+        hash = hash_combine(hash, std::hash<int64_t>()(value));
+    }
 }
 
 void AttrIntList::set(TF_OperationDescription *desc) const {
@@ -87,6 +115,8 @@ void AttrIntList::set(TF_OperationDescription *desc) const {
 }
 
 AttrFloat::AttrFloat(const std::string &name, float value) : Attr(name), value(value) {
+    hash = std::hash<std::string>()(name);
+    hash = hash_combine(hash, std::hash<float>()(value));
 }
 
 void AttrFloat::set(TF_OperationDescription *desc) const {
@@ -94,6 +124,10 @@ void AttrFloat::set(TF_OperationDescription *desc) const {
 }
 
 AttrFloatList::AttrFloatList(const std::string &name, const std::vector<float> &values) : Attr(name), values(values) {
+    hash = std::hash<std::string>()(name);
+    for (auto value : values) {
+        hash = hash_combine(hash, std::hash<float>()(value));
+    }
 }
 
 void AttrFloatList::set(TF_OperationDescription *desc) const {
@@ -101,6 +135,8 @@ void AttrFloatList::set(TF_OperationDescription *desc) const {
 }
 
 AttrBool::AttrBool(const std::string &name, bool value) : Attr(name), value(value) {
+    hash = std::hash<std::string>()(name);
+    hash = hash_combine(hash, std::hash<bool>()(value));
 }
 
 void AttrBool::set(TF_OperationDescription *desc) const {
@@ -112,6 +148,10 @@ void AttrBool::set(TF_OperationDescription *desc) const {
 }
 
 AttrBoolList::AttrBoolList(const std::string &name, const std::vector<bool> &values) : Attr(name), values(values) {
+    hash = std::hash<std::string>()(name);
+    for (auto value : values) {
+        hash = hash_combine(hash, std::hash<bool>()(value));
+    }
 }
 
 void AttrBoolList::set(TF_OperationDescription *desc) const {
@@ -121,6 +161,8 @@ void AttrBoolList::set(TF_OperationDescription *desc) const {
 }
 
 AttrString::AttrString(const std::string &name, const std::string &value) : Attr(name), value(value) {
+    hash = std::hash<std::string>()(name);
+    hash = hash_combine(hash, std::hash<std::string>()(value));
 }
 
 void AttrString::set(TF_OperationDescription *desc) const {
@@ -128,6 +170,10 @@ void AttrString::set(TF_OperationDescription *desc) const {
 }
 
 AttrStringList::AttrStringList(const std::string &name, const std::vector<std::string> &values) : Attr(name), values(values) {
+    hash = std::hash<std::string>()(name);
+    for (auto &value : values) {
+        hash = hash_combine(hash, std::hash<std::string>()(value));
+    }
 }
 
 void AttrStringList::set(TF_OperationDescription *desc) const {
@@ -141,6 +187,8 @@ void AttrStringList::set(TF_OperationDescription *desc) const {
 }
 
 AttrFuncName::AttrFuncName(const std::string &name, const std::string &func_name) : Attr(name), func_name(func_name) {
+    hash = std::hash<std::string>()(name);
+    hash = hash_combine(hash, std::hash<std::string>()(func_name));
 }
 
 void AttrFuncName::set(TF_OperationDescription *desc) const {
