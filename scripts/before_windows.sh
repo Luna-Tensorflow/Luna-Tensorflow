d72586fbaf37293f22eb5e746211db8871bfa369
Radosław Waśko <programistagd@gmail.com>
2019-03-12 22:46:49 +0100

    Fixed issue with travis.

--- scripts/before_windows.sh
+++ scripts/before_windows.sh
@@ -1,20 +1,20 @@
-- echo "Running Windows build"
-- mkdir tensorflow # download TF dependency, TODO downloading protobufs
-- cd tensorflow
-- curl -sS -o libtf.zip https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-windows-x86_64-1.12.0.zip
-- 7z x libtf.zip -y
-- chmod u+w lib/*
-- cd ..
-- mkdir build # prepare folders
-- mkdir release
-- mkdir release/native_libs
-- mkdir release/native_libs/linux/
-- cd scripts/ # download and patch the patching script
-- mv Main.hs Main2.hs
-- git clone https://github.com/luna/dataframes
-- mv dataframes/scripts/* .
-- mv Main2.hs Main.hs
-- cd ..
-- cd build
-- cmake -G "MinGW Makefiles" ../API/Tensorflow/native_libs/src/
-- cd ..
+echo "Running Windows build"
+mkdir tensorflow # download TF dependency, TODO downloading protobufs
+cd tensorflow
+curl -sS -o libtf.zip https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-windows-x86_64-1.12.0.zip
+7z x libtf.zip -y
+chmod u+w lib/*
+cd ..
+mkdir build # prepare folders
+mkdir release
+mkdir release/native_libs
+mkdir release/native_libs/linux/
+cd scripts/ # download and patch the patching script
+mv Main.hs Main2.hs
+git clone https://github.com/luna/dataframes
+mv dataframes/scripts/* .
+mv Main2.hs Main.hs
+cd ..
+cd build
+cmake -G "MinGW Makefiles" ../API/Tensorflow/native_libs/src/
+cd ..
