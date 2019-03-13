9b25483b5fd3de927294ce084b18223e72b33575
Radosław Waśko <programistagd@gmail.com>
2019-03-13 19:11:14 +0100

    Trying to fix Windows Travis

--- scripts/before_windows.sh
+++ scripts/before_windows.sh
@@ -1,9 +1,10 @@
 echo "Running Windows build"
+export TF_USE_LOCAL_LIBRARY=1
 mkdir tensorflow # download TF dependency, TODO downloading protobufs
 cd tensorflow
 curl -sS -o libtf.zip --insecure https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-windows-x86_64-1.12.0.zip
 7z x libtf.zip -y
-chmod u+w lib/*
+dir
 cd ..
 mkdir build # prepare folders
 mkdir release
