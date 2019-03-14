595ef5962547f7f233907a73de198129a087f7fe
Radosław Waśko <programistagd@gmail.com>
2019-03-14 18:03:14 +0100

    (#33) Fixed wrong tensorflow.dll path

--- scripts/deploy_windows.sh
+++ scripts/deploy_windows.sh
@@ -3,10 +3,10 @@ set -x
 cd build
 cmake --build .
 cd ../release/
-cp -r ../API/Tensorflow/src/ . # copy Luna srcs and metadata
-cp -r ../API/Tensorflow/.luna-package/ .
-cp -r ../API/Tensorflow/native_libs/windows native_libs/
-cp ../tensorflow/*.dll native_libs/windows/
+cp -rv ../API/Tensorflow/src/ . # copy Luna srcs and metadata
+cp -rv ../API/Tensorflow/.luna-package/ .
+cp -rv ../API/Tensorflow/native_libs/windows native_libs/
+cp ../tensorflow/lib/*.dll native_libs/windows/ -v
 7z a -t7z tensorflow_luna_windows.7z * .luna-package
 cd ..
 mkdir artifacts
