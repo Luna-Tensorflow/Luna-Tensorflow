9353192a3218accbf98f1f335de00ed40f73c7c4
Radosław Waśko <wasko.radek@gmail.com>
2019-04-08 23:44:17 +0200

    Quickfix for Macos issue

--- scripts/deploy_osx.sh
+++ scripts/deploy_osx.sh
@@ -8,7 +8,7 @@ stack run -- ../release/native_libs/macos/ ../API/Tensorflow/native_libs/macos/l
 cd ../release/
 cp -r ../API/Tensorflow/src/ . # copy Luna srcs and metadata
 cp -r ../API/Tensorflow/.luna-package/ .
-tar -cvf tensorflow_luna_osx.tar.gz .luna-package * # package it all
+tar -cvf tensorflow_luna_osx.tar.gz * # .luna-package * # package it all
 cd ..
 mkdir artifacts
 mv release/tensorflow_luna_osx.tar.gz artifacts/
