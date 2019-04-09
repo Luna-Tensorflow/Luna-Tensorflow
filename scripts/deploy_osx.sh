8fae5b0743ef18d447971c4c887485fd8f5db394
Wojciech Ciszewski <wojtrz13@gmail.com>
2019-04-09 21:48:48 +0200

    Mac build should work properly now

--- scripts/deploy_osx.sh
+++ scripts/deploy_osx.sh
@@ -6,9 +6,9 @@ cd ..
 cd scripts/ # patch binaries
 stack run -- ../release/native_libs/macos/ ../API/Tensorflow/native_libs/macos/libTFL.dylib
 cd ../release/
-cp -r ../API/Tensorflow/src/ . # copy Luna srcs and metadata
-cp -r ../API/Tensorflow/.luna-package/ .
-tar -cvf tensorflow_luna_osx.tar.gz * # .luna-package * # package it all
+cp -R ../API/Tensorflow/src . # copy Luna srcs and metadata
+cp -R ../API/Tensorflow/.luna-package .
+tar -cvf tensorflow_luna_osx.tar.gz .luna-package * # package it all
 cd ..
 mkdir artifacts
 mv release/tensorflow_luna_osx.tar.gz artifacts/
