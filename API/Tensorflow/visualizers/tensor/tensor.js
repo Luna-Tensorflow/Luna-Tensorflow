e7cb4c99809156ee94ee82cebad62ba3eda0887f
Radosław Waśko <wasko.radek@gmail.com>
2019-04-25 10:51:30 +0200

    (#103) Added 3d visualisations, general improvements.

--- API/Tensorflow/visualizers/tensor/tensor.js
+++ API/Tensorflow/visualizers/tensor/tensor.js
@@ -28,20 +28,40 @@
         return [json.length].concat(shape(json[0]));
     }
 
+    function rank(json) {
+        if (!Array.isArray(json)) {
+            return 0;
+        }
+        return 1 + rank(json[0]);
+    }
+
     function makeVis(vis) {
         document.body.innerHTML = //'<div class="container small">small vis TODO</div>' +
             '<div class="container big">' + vis + '</div>';
     }
 
-    function render2d(data) {
-        var sh = shape(data);
-        var w = sh[0];
-        var h = sh[1];
-        if (w == 0 || h == 0) {
-            return makeVis("Width or height is 0, shape: " + sh);
+    function findMinMax(data) {
+        if (Array.isArray(data)) {
+            var min = Infinity;
+            var max = -Infinity;
+            for (var x = 0; x < data.length; ++x) {
+                var stat = findMinMax(data[x]);
+                min = Math.min(min, stat.min);
+                max = Math.max(max, stat.max);
+            }
+            return {min: min, max: max};
+        } else {
+            return {min: data, max: data};
         }
-        function valueToColor(val) {
-            var v = Math.round(val * 255);
+    }
+
+    function blackAndWhiteMinMaxColoring(min, max) {
+        function rescale(x) {
+            return (x - min) / (max - min);
+        }
+
+        return function(v) {
+            v = Math.round(rescale(v) * 255);
             if (v > 255) {
                 v = 255;
             }
@@ -53,50 +73,119 @@
                 hex = "0" + hex;
             }
             return "#" + hex + hex + hex;
-        }
-        function trunc(x) {
-            return Math.round(100 * x) / 100;
-        }
-        var canvasSize = 200;
-        var side = canvasSize / Math.max(w,h);
+        };
+    }
 
-        document.body.innerHTML = '<div class="container big"><div id="debug"></div>debug info: ' + sh + ', ' + side + '<canvas id="viscanvas" width="400px" height="400px"></div>';
-        var canvas = document.getElementById("viscanvas");
+    function trunc(x) {
+        if (x == 0) {
+            return "0";
+        }
+        if (x > 0.01 && x < 10000) {
+            return "" + Math.round(100 * x) / 100;
+        } else {
+            return x.toExponential(3);
+        }
+    }
+
+    function renderToCanvas(canvas, data, valueToColor, addText) {
+        var sh = shape(data);
+        var w = sh[0];
+        var h = sh[1];
+
         var ctx = canvas.getContext("2d");
-        var dbg = "";
+        var canvasSize = 200;
+        ctx.fillStyle = '#BBCC00';
+        var side = (canvasSize - 4) / Math.max(w,h);
+        ctx.fillRect(0,0,4 + h * side,4 + w * side);
         for (var x = 0; x < w; ++x) {
             for (var y = 0; y < h; ++y) {
                 var v = data[x][y];
                 ctx.fillStyle = valueToColor(v);
-                ctx.fillRect(x * side, y * side, side, side);
-                if (side > 40) {
+                ctx.fillRect(y * side + 2, x * side + 2, side, side);
+                if (addText && side > 30) {
                     // for small pictures (so pixels are big), add text values
                     ctx.fillStyle = '#00AA00';
-                    ctx.fillText("" + trunc(v), x * side + 10, y * side + 10);
+                    ctx.fillText(trunc(v), y * side + 10, x * side + 15);
                 }
             }
         }
-        document.getElementById("debug").innerHTML = dbg;
-        return null;
+    }
+    function describe(sh, stat) {
+        return 'Shape: [' + sh + '], values range from ' + trunc(stat.min) + ' to ' + trunc(stat.max);
+    }
+    function render2d(data, coloring) {
+        var sh = shape(data);
+        var w = sh[0];
+        var h = sh[1];
+        if (w == 0 || h == 0) {
+            makeVis("Width or height is 0, shape: " + sh);
+            return;
+        }
+
+        var stat = findMinMax(data);
+        var valueToColor = coloring(stat.min, stat.max);
+
+        document.body.innerHTML = '<div class="container big">' + describe(sh, stat) + '<canvas id="viscanvas" width="400px" height="400px"></div>';
+        var canvas = document.getElementById("viscanvas");
+        renderToCanvas(canvas, data, valueToColor, true);
+    }
+
+    function render3dStacked(data, coloring) {
+        var sh = shape(data);
+        var html = "";
+        // TODO we want to stack along last not first dimension, this requires a reshape
+        for (var i = 0; i < data.length; ++i) {
+            html += '<div><canvas id="viscanvas' + i + '" width="200px" height="200px"></div><br>';
+        }
+        var stat = findMinMax(data);
+        document.body.innerHTML = '<div class="container big">' + describe(sh, stat) + html + '</div>';
+
+        var valueToColor = coloring(stat.min, stat.max);
+        for (var i = 0; i < data.length; ++i) {
+            var canvas = document.getElementById("viscanvas" + i);
+            renderToCanvas(canvas, data[i], valueToColor, true);
+        }
+    }
+
+    function renderRGB(data) {
+        var stat = findMinMax(data);
+        function rgbColor(v) {
+            function toHex(x) {
+                var hex = Math.round(255 * (x - stat.min) / (stat.max - stat.min)).toString(16);
+                if (hex.length < 2) {
+                    hex = "0" + hex;
+                }
+                return hex;
+            }
+            return "#" + toHex(v[0]) + toHex(v[1]) + toHex(v[2]);
+        }
+        document.body.innerHTML = '<div class="container big">' + '<canvas id="viscanvas" width="400px" height="400px"></div>';
+        var canvas = document.getElementById("viscanvas");
+        renderToCanvas(canvas, data, rgbColor);
     }
 
     var render = function (data) {
         var vis = JSON.stringify(data);
 
-        var sh = shape(data);
-        if (sh.length > 3) {
+        var r = rank(data);
+        if (r > 3) {
             return makeVis("Visualization of tensors of ranks higher than 3 are not supported right now");
         }
-        if (sh.length == 3) {
+        if (r == 3) {
             // TODO 3d visualization - show multiple 2d images side by side
+            if (shape(data)[2] == 3) {
+                renderRGB(data);
+            } else {
+                render3dStacked(data, blackAndWhiteMinMaxColoring);
+            }
         }
-        if (sh.length == 2) {
-            return render2d(data);
+        if (r == 2) {
+            return render2d(data, blackAndWhiteMinMaxColoring);
         }
-        if (sh.length == 1) {
-            return render2d([data]);
+        if (r == 1) {
+            return render2d([data], blackAndWhiteMinMaxColoring);
         }
-        if (sh.length == 0) {
+        if (r == 0) {
             return makeVis(data); // scalar value, no sense to show image
         }
         return "Error";
