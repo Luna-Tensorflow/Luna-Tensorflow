3d7674a4a003954492482389a6510eb387a8cdce
Radosław Waśko <wasko.radek@gmail.com>
2019-04-30 13:23:23 +0200

    Fixed Placeholders throwing errors in visualisation, they just return a string saying why it was not possible to visualize.
    (Without this every placeholder instance would show an error which is not intuitive)

--- API/Tensorflow/visualizers/tensor/config.js
+++ API/Tensorflow/visualizers/tensor/config.js
@@ -1,5 +1,5 @@
 module.exports = function (t) {
     var placeholderVis = {name: "tensor", path: "tensor.html"};
-    var types = ["Tensor"]; // TODO add other types like Layers etc.
+    var types = ["Tensor", "TFOutput"]; // TODO add other types like Layers etc.
     return types.includes(t.constructor) ? [placeholderVis] : [];
 };
