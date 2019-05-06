90d2b0b64ddd7f8101e768419619f02fc5e3d65c
Mateusz Goślinowski <mateusz.goslinowski@gmail.com>
2019-05-06 23:00:01 +0200

    quickfix16

--- scripts/before_windows.sh
+++ scripts/before_windows.sh
@@ -3,9 +3,22 @@ echo "Running Windows build"
 dir C:/ProgramData/chocolatey/bin/
 dir C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin
 export TF_USE_LOCAL_LIBRARY=1
+#zlib build
+mkdir zlib
+cd zlib
+curl -sS -o zlib.zip --insecure "https://zlib.net/zlib1211.zip"
+7z x zlib.zip -y
+dir
+cd "zlib-1.2.11"
+./configure
+cmake -G "MinGW Makefiles" -DCMAKE_SH=CMAKE_SH-NOTFOUND .
+cmake --build .
+cd ../..
+
+#libpng build
 mkdir libpng
 cd libpng
-curl -sS -o libpng.7z --insecure https://netix.dl.sourceforge.net/project/libpng/libpng16/1.6.37/lpng1637.7z
+curl -sS -o libpng.7z --insecure "https://netix.dl.sourceforge.net/project/libpng/libpng16/1.6.37/lpng1637.7z"
 7z x libpng.7z -y
 dir
 cd lpng1637
@@ -13,6 +26,8 @@ cd lpng1637
 cmake -G "MinGW Makefiles" -DCMAKE_SH=CMAKE_SH-NOTFOUND .
 cmake --build .
 cd ../..
+
+# tensorflow download
 mkdir tensorflow # download TF dependency, TODO downloading protobufs
 cd tensorflow
 curl -sS -o libtf.zip --insecure https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-windows-x86_64-1.12.0.zip
