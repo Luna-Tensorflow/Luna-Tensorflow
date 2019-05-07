c33b023d664b446af57dd70a1b560fc7c01ef69d
Mateusz Goślinowski <mateusz.goslinowski@gmail.com>
2019-05-07 20:04:41 +0200

    Small changes to Windows integration

--- scripts/deploy_windows.sh
+++ scripts/deploy_windows.sh
@@ -8,7 +8,7 @@ cp -rv ../API/Tensorflow/.luna-package/ .
 cp -v ../API/Tensorflow/native_libs/windows/libTFL.dll native_libs/windows/
 cp -v ../tensorflow/lib/tensorflow.dll native_libs/windows/libtensorflow.so # weird naming but that's what libTFL.dll requires
 cp -v ../zlib/zlib-1.2.11/libzlib.dll native_libs/windows/
-cp -v ../libpng/lpng1637/libpng16.dll native_libs/windows/libpng.dll
+cp -v ../libpng/lpng1637/libpng.dll native_libs/windows/
 cp -v C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin/libgcc_s_seh-1.dll native_libs/windows/
 cp -v C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin/libwinpthread-1.dll native_libs/windows/
 cp -v C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin/libstdc++-6.dll native_libs/windows/
